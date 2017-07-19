c586221d49492c5f3646e17932e465ed073036f2
Ashton Meuser <ashtonmeuser@gmail.com>
2017-07-19 16:30:40 -0700

    Test send bitmap via USB

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const Usb = require('./usb.js');
+// const Usb = require('./usb.js');
+const bitmapToGcode = require('./transform.js');
 const app = express();
-const usb = new Usb();
+// const usb = new Usb();
 
 app.use(express.static('public'));
-app.use(bodyParser.text());
+app.use(bodyParser.json({limit: '5mb'}));
 
 app.post('/upload', function (req, res) {
-  usb.send('G0 Z15');
+  if (typeof req.body.bitmap === 'undefined' ||
+    typeof req.body.bitmap.length === 'undefined' ||
+    req.body.bitmap.length !== 500) {
+    res.status(400);
+    res.send('Invalid bitmap');
+    return;
+  }
+  bitmapToGcode(req.body.bitmap, 100);
   res.send('Hello World!');
 });
 
 app.post('/send', function (req, res) {
-  usb.send(req.body);
-  res.send(req.body);
+  if (typeof req.body.gcode !== 'string') {
+    res.status(400);
+    res.send('Invalid G-code');
+    return;
+  }
+  usb.send(req.body.gcode);
+  res.send(req.body.gcode);
 });
 
 app.listen(3000, function () {
---
+++ transform.js
@@ -0,0 +1,276 @@
+const Usb = require('./usb.js');
+const usb = new Usb();
+
+var BMX_TO_DEGREES = 360/4096; //Conversion ratio between stepper steps per rotation to degrees
+var Z_STEPS_TO_MM = 0.01; //*** Need to calculate this value
+var XY_STEPS_TO_MM = 0.01; //*** Need to calculate this value
+var MAX_Z = 300.0; //*** Need to calculate this value
+var SPI_STR_LENGTH = 50; //*** don't know what the grbl fimware limit is
+var ROW_WIDTH = 8; //*** need to find correct value for this *** possibly don't need them???
+var LASER_FOCUS_DISTANCE = 100; //100 mm *** testing needed to find correct value
+var ABS_X_CENTER_COOR = 500; //*** Still need to find out this value
+var ABS_Y_CENTER_COOR = 500; //*** Still need to find out this value
+var TRUE = 1;
+var FALSE = 0;
+
+//Variables
+var txbuf = new Buffer.alloc(50); //Buffer's used to send and receive with SPI
+var rxbuf = new Buffer.alloc(50);
+
+//***test Degrees:	0	45	90	135	180	225	270	315
+// inverse			180	225	270	315	0	45	90	135
+//				    0  	1  	2  	3  	4  	5  	6  	7
+var testBitmap = [ [0, 	0, 	1, 	1, 	0,  0,  1, 	1], //0
+     [0, 	0, 	0, 	0, 	0,  0, 	0, 	0], //1
+     [2, 	2, 	0, 	0, 	0, 	0, 	3, 	3], //2
+     [3, 	3, 	0, 	0, 	0, 	0, 	4, 	4], //3
+     [5, 	5, 	0, 	0, 	0, 	0, 	0,  0]	//4
+   ];
+
+//Function to send a string (gCode) directly to the STM32f0 (TODO console like feature on web app)
+function gcodeConsole(gcode){
+  SPIsendString(gcode);
+}
+
+/* 	configEngraver
+
+  Description: Based on if there is a diameter argument or not it will send a gCode
+  string the configures the engraver with the appropriate settings.
+
+*/
+function configEngraver(diameter){
+  var txString = "";
+
+
+  if(typeof(diameter === 'undefined')) { //Plane
+  //*** Still need to figure out how to correctly set coordinate system
+  txString = "S0\n$H\nG90.1\nM4"; //***Needs to be tested
+  SPIsendString(txString); //*** needs to be changed to UART comms
+  //if(rxbuf)...//TODO Check for errors from stm32f0, send rx messages to webapp?
+  }
+  else{ //Cylinder
+
+  txString = "S0\n$H\nG17\nG91.1\nM4"; //***Needs to be tested
+  SPIsendString(txString); //*** needs to be changed to UART comms
+  //if(rxbuf)...//TODO Check for errors from stm32f0, send rx messages to webapp?
+
+  //DEBUG
+  //console.log(txString);
+  }
+}
+
+/*	bitmapTocode
+
+  Description:Receives a 2d array (bitmap) and parses through it creating gcode scripts that it sends over SPI
+  to a STM32f0. What the parsing does is find any changes in value and tells the laser to go to the appropiate
+  location while also deciding what power level to set the laser at for that move.
+
+  Arguments:
+  bitMap - 2D array of chars (value 0-255) of size *** (hasn't been defined yet)
+  height - float that indicates the height of the object that is being engraved in mm
+  diameter - float - only should receive this parammeter when a cylindrical object is being engraved. in mm
+*/
+
+function bitmapToGcode(bitMap, height, diameter){
+  var txString ="";
+  var x=0,y=0,z=0,power=0,i=0,j=0;
+  var previousValue = 0,previousRow=0;
+  var laserOn = 0; //Variable to track what state the laser is at
+
+  //This double for loop will send commands to the stm32f0 whenever it reaches a change of state as it.
+  console.log(typeof(diameter));
+  if(typeof(diameter) === 'undefined'){ //Plane
+  z = (height + LASER_FOCUS_DISTANCE)*Z_STEPS_TO_MM; //For plane the z is a constant height
+  //TODO improve first iteration where it will move in slow mode (G1) whether the laser is on or off.
+  console.log("Planar Mode:");
+
+  for(var bmY=0; bmY<bitMap.length; bmY++){
+  for(var bmX=0; bmX<bitMap[0].length; bmX++){
+
+  //Check for a change in power level
+  if(bitMap[bmY][bmX] !== previousValue){
+
+  // When moving to the next row start start at x=0
+  if(bmY !== previousRow && bitMap[bmY][bmX] > 0 ){
+  bmX = 0;
+  x = 0;
+  }
+  else{
+  x = bmX * XY_STEPS_TO_MM;
+  }
+
+  previousRow = bmY;
+
+  if(bitMap[bmY][bmX] > 0 && previousValue === 0){
+  laserOn = FALSE;
+  }
+  else{
+  laserOn = TRUE;
+  }
+  y = bmY * XY_STEPS_TO_MM;
+  if(bmX === 0){
+  power = 0;
+  }
+  else{
+  power = laserOn*bitMap[bmY][(bmX-1)];
+  }
+
+  txString = "G"+laserOn+"X"+x+"Y"+y+"Z"+z+"S"+power;
+
+  SPIsendString(txString);
+  console.log(txString);
+  }
+  //Checks if a row ends with power >0 fixing a bug that caused the code to not create a path
+  //for the laser if the path ended on the end of a row
+  else if(bmX === (ROW_WIDTH-1) && bitMap[bmY][bmX] > 0){
+  x = ROW_WIDTH-1 * XY_STEPS_TO_MM;
+  y = bmY * XY_STEPS_TO_MM;
+  power = bitMap[bmY][(bmX)];
+  txString = "G"+1+"X"+x+"Y"+y+"Z"+z+"S"+power;
+  console.log(txString);
+  SPIsendString(txString); //*** needs to be changed to UART comms
+  }
+  //Checks if there has been change of rows and now change in power (>0)
+  else if(bmX === 0 && previousValue > 0){
+  x = 0;
+  y = bmY * XY_STEPS_TO_MM;
+  power = 0;
+  txString = "G"+0+"X"+x+"Y"+y+"Z"+z+"S"+power;
+  console.log(txString);
+  SPIsendString(txString); //*** needs to be changed to UART comms
+  }
+  previousValue = bitMap[bmY][bmX];
+  }
+  }
+  }
+
+  //-----------------------------
+
+  else{ //Cylinder
+  var radius = diameter/2 + LASER_FOCUS_DISTANCE;
+  x = radius;
+  y = 0;
+  a = 180;
+  i = ABS_X_CENTER_COOR - x; //the starting center coordinates (relative to starting location)
+  j = ABS_Y_CENTER_COOR - y; //the starting center coordinates (relative to starting location)
+
+  //Move to starting point (straight to the right of the top of the object)
+  txString = "G0X" + radius + "Y0Z" + height + "E0";
+  SPIsendString(txString); //*** needs to be changed to UART comms
+  console.log("Cylindrical mode:");
+  console.log(txString);
+
+  for(var bmZ=0; bmZ<bitMap.length; bmZ++){
+  for(let bmX=0; bmX<bitMap[0].length; bmX++){
+  //Check for a change in power level
+  if(bitMap[bmZ][bmX] !== previousValue){
+  if(bmZ !== previousRow && bitMap[bmZ][bmX] > 0 ){
+  bmX = 0;
+  x = radius * XY_STEPS_TO_MM;
+  y = 0;
+  a = 180;
+  }
+  else{
+  x = (radius*Math.cos(bmX*BMX_TO_DEGREES*Math.PI/180)).toFixed(3) * XY_STEPS_TO_MM; //*** what is the max deccimal points for grbl???
+  y = (radius*Math.sin(bmX*BMX_TO_DEGREES*Math.PI/180)).toFixed(3) * XY_STEPS_TO_MM;//***
+  a = (bmX*BMX_TO_DEGREES+180)%360;
+  }
+
+  //				   y+
+  //				   |     * End Point
+  //				   |    /^
+  //				   |   / |
+  //				   |  /  | radius*sin(bmX)    (bmX = angle)
+  //				   | /   |
+  //				   |/    |
+        		//		 ----------|----->-------*-- x+
+  //				   |  ^		     Start point
+  //				   |  |
+  //				   |  radius*cos(bmX)
+
+  z = height - bmZ * Z_STEPS_TO_MM;
+
+  ///////// Set Power
+
+  //When a difference in power in the bitmap is found this algorithm tells the laser to go to that
+  //location. So when there is a positive differential the laser needs to be off as it goes to that location
+  if(bitMap[bmZ][bmX] > 0 && previousValue === 0){
+  laserOn = FALSE;
+  }
+  else{
+  laserOn = TRUE;
+  }
+
+  //Ensures that when moving to a next row the power is not on (to avoid undesired points being engraved)
+  if(bmX === 0){
+  power = 0;
+  }
+  else{
+  power = laserOn*bitMap[bmZ][(bmX-1)];
+  }
+  //Send G-code with xyz value and the relative center of rotation
+  //power is turned on when laserOn is true.
+
+ 					txString = "G3X"+x+"Y"+y+"Z"+z+"A"+a+"I"+i+"J"+j+"S"+power;
+  SPIsendString(txString); //*** needs to be changed to UART comms
+
+  //Calculate the relative center location for next move
+  i = ABS_X_CENTER_COOR - x;
+  j = ABS_Y_CENTER_COOR - y;
+
+  previousRow = bmZ;
+
+  console.log(txString);
+  }
+  //Checks if a row ends with a power >0 fixing a bug that caused the code to not create a path
+  //for the laser that went to the end
+  else if(bmX === (ROW_WIDTH-1) && bitMap[bmZ][bmX] > 0){
+  //*** To do it this way we need to have the software not include the last column of resolution
+  //that the hardware is capable of doing.
+  x = (radius*Math.cos(bmX*BMX_TO_DEGREES*Math.PI/180)).toFixed(3) * XY_STEPS_TO_MM;
+  y = (radius*Math.sin(bmX*BMX_TO_DEGREES*Math.PI/180)).toFixed(3) * XY_STEPS_TO_MM;
+  z = height - bmZ * Z_STEPS_TO_MM;
+  a = (bmX*BMX_TO_DEGREES+180)%360;
+  power = bitMap[bmZ][(bmX)];
+  txString = "G3X"+x+"Y"+y+"Z"+z+"A"+a+"I"+i+"J"+j+"S"+power;
+  console.log(txString);
+  SPIsendString(txString); //*** needs to be changed to UART comms
+
+  //Calculate the relative center location for next move
+  i = ABS_X_CENTER_COOR - x;
+  j = ABS_Y_CENTER_COOR - y;
+
+  }
+  //Checks if there has been change of rows and in power (>0)
+  else if(bmX === 0 && previousValue > 0){
+  x = (radius*Math.cos(bmX*BMX_TO_DEGREES*Math.PI/180)).toFixed(3) * XY_STEPS_TO_MM; //*** what is the max deccimal points for grbl???
+  y = (radius*Math.sin(bmX*BMX_TO_DEGREES*Math.PI/180)).toFixed(3) * XY_STEPS_TO_MM;//*** what is the max for grbl???
+  z = height - bmZ * Z_STEPS_TO_MM;
+  a = 180;
+  power = 0;
+  txString = "G3 X"+x+"Y"+y+"Z"+z+"A"+a+"I"+i+"J"+j+"S"+power;
+  console.log(txString);
+  SPIsendString(txString); //*** needs to be changed to UART comms
+
+  //Calculate the relative center location for next move
+  i = ABS_X_CENTER_COOR - x;
+  j = ABS_Y_CENTER_COOR - y;
+  }
+  previousValue = bitMap[bmZ][bmX];
+  }
+  }
+  }
+}
+
+
+function SPIsendString(txString){
+  usb.send(txString);
+  console.log(txString);
+  return;
+  txbuf.fill(0); //Clear Buffers
+  rxbuf.fill(0);
+  rpio.spiTransfer(txbuf.fill(txString,0,txString.length), rxbuf, SPI_STR_LENGTH);
+  // TODO ***Error checking with rxbuf...
+}
+
+module.exports = bitmapToGcode;
