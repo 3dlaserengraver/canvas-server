7274075e2f26295724cf294cbc94ca79fef451e1
Ashton Meuser <ashtonmeuser@gmail.com>
2017-07-19 15:28:34 -0700

    Route for sending raw G code at /send

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Usb = require('./usb.js');
 const app = express();
 const usb = new Usb();
 
 app.use(express.static('public'));
+app.use(bodyParser.text());
 
 app.post('/upload', function (req, res) {
   usb.send('G0 Z15');
   res.send('Hello World!');
+});
+
+app.post('/send', function (req, res) {
+  usb.send(req.body);
+  res.send(req.body);
 });
 
 app.listen(3000, function () {
