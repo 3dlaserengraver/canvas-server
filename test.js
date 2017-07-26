482a4b00caf43ee8e1162eaa93b44eef1777128f
caleb <caleb.schulz@gmail.com>
2017-07-26 10:22:47 -0700

    Testing planar and cylindrical functions

--- test.js
+++ test.js
@@ -1,21 +1,22 @@
-//const bodyParser = require('body-parser');
-//const Usb = require('./usb.js');
-//const async = require('async');
+const bodyParser = require('body-parser');
+const Usb = require('./usb.js');
+const async = require('async');
 const testBitmaps = require('./testBitmaps.js');
 
-//const usb = new Usb();
+const usb = new Usb();
 
 const Gcode = require('./gcode.js');
 const gcode = new Gcode();
 
 let gcodeArray = [];
 
-console.log(gcode.startup());
+//console.log(gcode.startup());
 
-console.log(gcode.moveToStart(100,50));//planar
+
+//console.log(gcode.moveToStart(100,50));//planar
 //console.log(gcode.moveToStart(100,50,10));//cylindrical
 
-gcodeArray = gcode.planar(testBitmaps[0], 10, 100);
+gcodeArray = gcode.planar(testBitmaps[1], 0, 100);
 //gcodeArray = gcode.cylindrical(gcode.testBitmap, 10, 100);
 
 gcodeArray.unshift('M3S0') //turn laser on
